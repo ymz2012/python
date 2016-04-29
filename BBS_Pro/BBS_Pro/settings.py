17c7b2eb4f06b8330e9e3f5b3134471e89205a58
yumingzhi <yumingzhi@dayima.com>
2016-04-29 18:39:43 +0800

    111

--- BBS_Pro/BBS_Pro/settings.py
+++ BBS_Pro/BBS_Pro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.sites',
 )
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- BBS_Pro/app01/admin.py
+++ BBS_Pro/app01/admin.py
@@ -1,3 +1,16 @@
+#-*- coding:utf-8 -*-
 from django.contrib import admin
-
+from app01 import models
 # Register your models here.
+class BBS_admin(admin.ModelAdmin):
+    list_display=('title','summary','author','signature','view_count','created_at')
+    list_filter=('created_at',)
+    search_fields =('title','author__user__username')
+    
+    def signature(self,obj):
+        return obj.author.signature
+    signature.short_description  = '描述'
+
+admin.site.register(models.BBS,BBS_admin)
+admin.site.register(models.Category)
+admin.site.register(models.BBS_user)
